05cf416662e5ea09114602a7328bd4f454539d6e
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-06 19:01:16 +0200

    light and dark mood and added header component will be updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from './Components/side-bar/side-bar';
-
+import { ThemeService } from './Core/Services/theme';
+import { ThemeToggle } from "./Components/theme-toggle/theme-toggle";
+import { Footer } from "./Components/footer/footer";
+import { Header } from "./Components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThemeToggle, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('saas');
+  themeService = inject(ThemeService);
 }
