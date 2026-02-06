8e10219e78a1ceedb5fb3c2d00ad243247c48922
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-06 14:38:17 +0200

    add about and contact componsnts inti

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './Components/home/home';
 import { Login } from './Components/login/login';
@@ -7,11 +8,11 @@ import { Categories } from './Components/categories/categories';
 import { ForgetPassword } from './Components/forget-password/forget-password';
 import { Profile } from './Components/profile/profile';
 import { NotFounded } from './Components/not-founded/not-founded';
+import { About } from './Components/about/about';
+import { Contact } from './Components/contact/contact';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', component: Register },
-
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
@@ -20,5 +21,7 @@ export const routes: Routes = [
   { path: 'categories/:catSlug', component: Categories },
   { path: 'categories', component: Categories },
   { path: 'forget-password', component: ForgetPassword },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
   { path: '**', component: NotFounded },
 ];
