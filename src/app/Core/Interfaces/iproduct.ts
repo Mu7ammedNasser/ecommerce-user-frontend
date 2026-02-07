c82bc73127ccdac40d3c14cd192587b588f65fdb
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-07 16:28:07 +0200

    for terpasy

--- src/app/Core/Interfaces/iproduct.ts
+++ src/app/Core/Interfaces/iproduct.ts
@@ -1,12 +1,14 @@
 export interface Product {
   id: number;
   title: string;
+  description: string;
   category: string;
   categoryLabel: string;
   imgUrl: string;
   price: number;
   originalPrice: number;
   rating: number;
+  quantity: number;
   reviews: number;
   onSale: boolean;
 }
