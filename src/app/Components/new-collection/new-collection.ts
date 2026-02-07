1ae85deb55ac59d4e59271a84b3a693c035f998a
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-07 02:41:10 +0200

    3amalt 4ouet 7agat

--- src/app/Components/new-collection/new-collection.ts
+++ src/app/Components/new-collection/new-collection.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-new-collection',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-collection.html',
   styleUrl: './new-collection.css',
 })
--- src/app/Components/seasonal-sale/seasonal-sale.ts
+++ src/app/Components/seasonal-sale/seasonal-sale.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-seasonal-sale',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './seasonal-sale.html',
   styleUrl: './seasonal-sale.css',
 })
