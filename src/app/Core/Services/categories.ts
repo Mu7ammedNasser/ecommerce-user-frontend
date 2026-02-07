7e0a7c18ffcd8326ea85e9041aeba86a7e987d0e
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-07 14:26:34 +0200

    categories working with fake api

--- src/app/Core/Services/categories.ts
+++ src/app/Core/Services/categories.ts
@@ -1,8 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../Interfaces/iproduct';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Categories {
-  
+  baseUrl = 'http://localhost:3000';
+
+  constructor(private HttpClient: HttpClient) {}
+  // get all products
+
+  getAllProducts(): Observable<Product[]> {
+    return this.HttpClient.get<Product[]>(`${this.baseUrl}/products`);
+  }
+
+  //get Product by category
+  getProductsByCategory(category: string) {
+    return this.HttpClient.get<Product[]>(`${this.baseUrl}/products?category=${category}`);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.HttpClient.get<Product>(`${this.baseUrl}/products/${id}`);
+  }
 }
